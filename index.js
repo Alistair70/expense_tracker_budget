1b34e6d6d5f40933029ba8b565c729aa1e71e2e8
Alistair70 <63516216+Alistair70@users.noreply.github.com>
2024-02-01 14:23:23 -0500

    logout button full update

--- index.js
+++ index.js
@@ -2,11 +2,12 @@
 document.getElementById("dashButton").addEventListener("click", function() {
     window.location.href = "https://dashboard.expense-tracker-demo.site";
 });
-
 document.getElementById("logout").addEventListener("click", function() {
     cookie_name = "expense_tracker_cookie_container"
-    document.cookie = `${cookie_name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;`;
-    window.location.href = 'https://expense-tracker-aytr.onrender.com';
+    const now = new Date();
+    const expirationTime = new Date(now.getTime() - 15 * 60 * 1000);
+    document.cookie = `${cookie_name}=; domain=.expense-tracker-demo.site; expires=${expirationTime.toUTCString()}; path=/`;
+    window.location.href = 'https://landing.expense-tracker-demo.site/';
 });
 
 encoded_id = getEncodedID_or_Landing();
